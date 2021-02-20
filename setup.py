05ad6b44d4f0408711b99d60f2fb55c786957c65
David Alami <david.alami@gmail.com>
2021-02-20 09:55:15 +0200

    Extended supported python versions. Closes #1.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="conversational-sentence-encoder",
-    version="0.0.4",
+    version="0.0.5",
     author="David Alami",
     author_email="davidalami@gmail.com",
     description="Dual sentence encoder package",
@@ -26,5 +26,5 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='==3.6.*',
+    python_requires='>=3.6.0, <3.8.0',
 )
